1816b9e5b0136d8d7238fc7a30e7b346a09fd82a
JaxNelson17 <jaxnel17@gmail.com>
2021-10-06 16:02:20 -0600

    got a working rcview
    time for a merge

--- app/src/main/java/com/example/dnd5eplayerapp/MainActivity.kt
+++ app/src/main/java/com/example/dnd5eplayerapp/MainActivity.kt
@@ -4,13 +4,18 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.AdapterListUpdateCallback
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.dnd5eplayerapp.repository.Repository
+import com.example.dnd5eplayerapp.ui.home.HomeAdapter
 import kotlinx.coroutines.launch
 
-
-
-
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: HomeAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.home_fragment)
@@ -23,5 +28,11 @@ class MainActivity : AppCompatActivity() {
                 Log.i("menu", "menu is ${response.body()}")
             }
         }
+
+        recyclerView = findViewById(R.id.home_view)
+        adapter = HomeAdapter()
+
+        recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        recyclerView.adapter = adapter
     }
 }
--- app/src/main/java/com/example/dnd5eplayerapp/ui/home/HomeViewModelFactory.kt
+++ app/src/main/java/com/example/dnd5eplayerapp/ui/home/HomeViewModelFactory.kt
@@ -1,4 +1,17 @@
 package com.example.dnd5eplayerapp.ui.home
 
-class HomeViewModelFactory {
+import android.app.Application
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+class HomeViewModelFactory(
+    private val application: Application
+) : ViewModelProvider.Factory {
+    @Suppress("unchecked_cast")
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(HomeViewModel::class.java)) {
+            return HomeViewModel(application) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
 }
