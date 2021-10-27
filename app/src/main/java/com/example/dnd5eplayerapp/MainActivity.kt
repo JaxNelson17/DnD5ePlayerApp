cb627e37c102a19cbde38e5be353a08c6cea644f
JaxNelson17 <jaxnel17@gmail.com>
2021-10-27 16:12:45 -0600

    got the navigation working

--- app/src/main/java/com/example/dnd5eplayerapp/MainActivity.kt
+++ app/src/main/java/com/example/dnd5eplayerapp/MainActivity.kt
@@ -5,12 +5,25 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.example.dnd5eplayerapp.ui.home.HomeFragment
 import com.example.dnd5eplayerapp.ui.mainMenu.MainMenuFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        
+
+        val home = HomeFragment()
+        val menu = MainMenuFragment()
+
+        makeCurrentFragment(menu)
+
+        bottom_nav_bar.setOnItemSelectedListener {
+            when (it) {
+                R.id.navigation_home -> makeCurrentFragment(menu)
+                R.id.navigation_menu -> makeCurrentFragment(home)
+            }
+        }
+
     }
     private fun makeCurrentFragment(fragment: Fragment) =
         supportFragmentManager.beginTransaction().apply {
