bb7c8bc280750d99eae3f1608a2f53fae71f614b
JaxNelson17 <jaxnel17@gmail.com>
2021-10-28 16:13:01 -0600

    able to see the layout

--- app/src/main/java/com/example/dnd5eplayerapp/ui/characterCreation/CreationFragment.kt
+++ app/src/main/java/com/example/dnd5eplayerapp/ui/characterCreation/CreationFragment.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.dnd5eplayerapp.R
 import com.example.dnd5eplayerapp.databinding.CreationFragmentBinding
@@ -15,7 +17,7 @@ class CreationFragment() : Fragment() {
     private lateinit var viewModel: CreationViewModel
     private lateinit var binding: CreationFragmentBinding
 
-    override fun onCreate(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
 
         binding = DataBindingUtil.inflate(inflater, R.layout.creation_fragment, container, false)
@@ -26,6 +28,11 @@ class CreationFragment() : Fragment() {
         viewModel = ViewModelProvider(this, viewModelFactory).get(CreationViewModel::class.java)
 
         binding.creationViewModel = viewModel
+
+         val textView: TextView = binding.mainTextView
+         viewModel.text.observe(viewLifecycleOwner, Observer {
+             textView.text = it
+         })
 
         return binding.root
     }
--- app/src/main/java/com/example/dnd5eplayerapp/ui/characterCreation/CreationViewModel.kt
+++ app/src/main/java/com/example/dnd5eplayerapp/ui/characterCreation/CreationViewModel.kt
@@ -1,7 +1,14 @@
 package com.example.dnd5eplayerapp.ui.characterCreation
 
 import android.app.Application
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class CreationViewModel(application: Application) :  ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "Welcome to the Character Creation Screen!!"
+    }
+    val text: LiveData<String> = _text
 }
