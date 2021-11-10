e7cbb61d9ddd520dff47df436f4947922a882f88
JaxNelson17 <jaxnel17@gmail.com>
2021-11-10 13:53:21 -0700

    got it working
    got all the dice able to roll and there is a button that will navigate back to the main menu

--- app/src/main/java/com/example/dnd5eplayerapp/ui/diceRoller/DiceRollerFragment.kt
+++ app/src/main/java/com/example/dnd5eplayerapp/ui/diceRoller/DiceRollerFragment.kt
@@ -7,9 +7,9 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.example.dnd5eplayerapp.R
 import com.example.dnd5eplayerapp.databinding.DiceRollerBinding
-import com.example.dnd5eplayerapp.ui.characterCreation.CreationViewModel
 
 class DiceRollerFragment : Fragment() {
 
@@ -30,6 +30,38 @@ class DiceRollerFragment : Fragment() {
         viewModel = ViewModelProvider(this, viewModelFactory).get(DiceRollerViewModel::class.java)
 
         binding.diceRollerViewModel = viewModel
+
+        binding.d20Roll.setOnClickListener {
+            binding.d20Result.text = viewModel.rollD20().toString()
+        }
+
+        binding.d12Roll.setOnClickListener {
+            binding.d12Result.text = viewModel.rollD12().toString()
+        }
+
+        binding.d10Roll.setOnClickListener {
+            binding.d10Result.text = viewModel.rollD10().toString()
+        }
+
+        binding.d10Roll2.setOnClickListener {
+            binding.d10Result2.text = viewModel.rollD10Two().toString()
+        }
+
+        binding.d8Roll.setOnClickListener {
+            binding.d8Result.text = viewModel.rollD8().toString()
+        }
+
+        binding.d6Roll.setOnClickListener {
+            binding.d6Result.text = viewModel.rollD6().toString()
+        }
+
+        binding.d4Roll.setOnClickListener {
+            binding.d4Result.text = viewModel.rollD4().toString()
+        }
+
+        binding.navBack.setOnClickListener {
+            findNavController().navigate(R.id.mainMenuFragment)
+        }
 
         return binding.root
     }
